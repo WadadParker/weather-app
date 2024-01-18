8a0fe53a30934c9852f606fb6dd37743d079c505
Wadad Parker <wadadparker5@gmail.com>
2024-01-18 07:12:09 +0530

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import NavBar from "./components/NavBar";
+
+const apiKey = import.meta.env.VITE_API_KEY;
 
 function App() {
 
   return (
 
-      <div>
-        <h1>Hello world</h1>
-      </div>
+    <div className=" font-roboto">
+      <NavBar />
+      <h1>Hello world</h1>
+    </div>
 
   )
 }
